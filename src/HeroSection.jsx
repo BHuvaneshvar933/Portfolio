831a53d7b1e57ac4778be024251bb16f64643501
BHuvaneshvar933 <bhuvaneshvar.r.c@gmail.com>
2025-04-06 18:28:09 +0530

    updated

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,39 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const HeroSection = () => (
   <section
     id="home"
-    className=" w-full hero bg-gradient-to-r text-white h-screen flex flex-col justify-center items-center text-center relative px-7 mt-10"
+    className="w-full hero bg-gradient-to-r text-white h-screen flex flex-col justify-center items-center text-center relative px-7 mt-24"
   >
     <h1
       className="text-5xl sm:text-6xl font-extrabold leading-tight mb-6 fadeIn"
       id="heading"
     >
-      C BHUVANESHVAR
+      HELLO I'M C BHUVANESHVAR
       <span className="bg-gradient-to-r from-purple-300 to-purple-600 text-transparent bg-clip-text font-extrabold">
-        {" "}
-        REDDY
+        {" "}REDDY
       </span>
     </h1>
+
     <p
-      className="text-lg sm:text-xl mb-8 mx-auto slideUp text-center text-neutral-400 max-w-4xl font-bold"
+      className="text-lg sm:text-xl mb-6 mx-auto slideUp text-center text-neutral-400 max-w-4xl font-bold"
       id="normalText"
     >
-      I'm a CSE Student at Cambridge Institute of Technology specializing in Software and Web Development
-      <span className="bg-gradient-to-r from-purple-400 to-purple-700 text-transparent bg-clip-text"></span>
+      I'm a web developer and freelance web designer with experience building responsive, user-focused websites using React, Tailwind CSS, and MongoDB. I've worked with real clients, including e-commerce and portfolio projects, delivering functional and visually appealing websites. I'm currently seeking a web development internship where I can continue to grow and contribute in a team environment.
     </p>
+
+    {/* Resume Button */}
+    <a
+      href="/C_Bhuvaneshvar_Resume.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+       className="px-4 mt-4 py-3 w-full md:w-40 shadow-md hover:bg-purple-700 transition bg-white rounded-full text-lg font-semibold hoverScale bg-gradient-to-r from-purple-500 to-purple-900 text-white shadow-neon hover:shadow-neon-hover"
+            id="neon-button"
+    >
+      View Resume
+    </a>
+
+ 
     <img
       src="https://mattfarley.ca/img/mf-avatar.svg"
-      className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-40"
+      className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-40 mt-6"
       alt="Avatar"
     />
+
     
     <a href="#work">
-      <div className="scroll-indicator absolute top-28 md:top-34 mb-24 bg-gradient-to-r from-purple-300 to-purple-600 text-transparent bg-clip-text font-extrabold ">
-      <p>My work</p>
-      </div>
+      <button className="px-4 mt-10 py-3 w-full md:w-40 shadow-md hover:bg-purple-700 transition bg-white rounded-full text-lg font-semibold hoverScale bg-gradient-to-r from-purple-500 to-purple-900 text-white shadow-neon hover:shadow-neon-hover"
+            id="neon-button">
+        View My Work
+      </button>
     </a>
-   
+<Link to="/user-form">
+    <a >
+      <p className="mt-4 text-purple-300 underline hover:text-purple-100 transition">
+        Let’s Connect
+      </p>
+    </a>
+  </Link>
   </section>
 );
 
--- src/Work.jsx
+++ src/Work.jsx
@@ -15,28 +15,36 @@ const Work = () => {
     {
       title: "Event Management System",
       description: "A complete event registration and management platform with user login, QR check-in, and admin dashboard.",
+      Tech_Stack: "React.js, Tailwind CSS, MongoDB",
       image: event,
+      demo:"https://event-management-dusky.vercel.app",
       link: "https://github.com/BHuvaneshvar933/Event-Management",
       domain: "Web development"
     },
     {
       title: "RecipeSauce",
       description: "A dynamic recipe search engine using an API to fetch thousands of recipes based on ingredients or meal types.",
+      Tech_Stack: "React.js, Tailwind CSS, API",
       image: recipesauce,
+      demo:"https://recipe-sauce.vercel.app",
       link: "https://github.com/BHuvaneshvar933/RecipeSauce",
        domain: "Web development"
     },
     {
       title: "Airbnb Clone",
       description: "A clone of Airbnb's feed and profile system with user authentication and post interactions.",
+      Tech_Stack: "React.js, Tailwind CSS, MongoDB",
       image: air,
+      demo:"https://ease-nest.netlify.app",
       link: "https://github.com/BHuvaneshvar933/AirBnb_clone",
        domain: "Web development"
     },
     {
       title: "E-commerce Gym Website",
       description: "A modern gym store platform with product catalog, details, and cart functionality using React and MongoDB.",
+      Tech_Stack: "React.js, Tailwind CSS, MongoDB",
       image: gym,
+      demo:"gymstore3.vercel.app",
       link: "https://github.com/BHuvaneshvar933/gymstore3",
        domain: "Web development"
     },
@@ -44,13 +52,17 @@ const Work = () => {
       title: "Photography Portfolio",
       description: "A clean and minimalist portfolio website for photographers to showcase their work with elegant design.",
       image: home,
+      Tech_Stack: "HTML, CSS , JavaScript",
+      demo:"https://photography-portfolio-ebon-seven.vercel.app",
       link: "https://github.com/BHuvaneshvar933/Photography_portfolio",
        domain: "Web development"
     },
     {
       title: "Virtual Reality Landing Page",
       description: "A stylish landing page for a VR product, highlighting features, benefits, and call-to-action sections.",
+      Tech_Stack: "React.js, Tailwind CSS",
       image: vreality,
+      demo:"https://v-reality-azure.vercel.app",
       link: "https://github.com/BHuvaneshvar933/vreality",
        domain: "Web development"
     },
@@ -58,6 +70,7 @@ const Work = () => {
     {
       title: "Chat App",
       description: "A real-time chat application using WebSockets where users can create rooms and chat instantly.",
+      Tech_Stack: "React.js, Tailwind CSS, MongoDB",
       image: chat,
       link: "https://github.com/BHuvaneshvar933/cha",
        domain: "Web development"
@@ -87,7 +100,7 @@ const Work = () => {
               className="rounded-lg overflow-hidden shadow-md transition-transform"
             >
               <a
-                href={project.link}
+                
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block"
@@ -100,17 +113,38 @@ const Work = () => {
                     className="object-cover w-full h-full transform transition-transform duration-500 group-hover:scale-105"
                   />
                 </div>
-                <div className="p-4 bg-gradient-to-br from-neutral-950 to-neutral-800 rounded-lg h-48">
+                <div className="p-4 bg-gradient-to-br from-neutral-950 to-neutral-800 rounded-lg ">
                   <h3 className="text-xl sm:text-lg md:text-2xl  bg-gradient-to-r from-purple-400 to-purple-600 text-transparent bg-clip-text font-extrabold">
                     {project.title}
                   </h3>
                   <h3 className="w-12 mt-2 h-1 bg-neutral-300 rounded-lg"></h3>
+                  <p className="text-sm mt-2 sm:text-base md:text-base text-gray-400">
+                    {project.Tech_Stack}
+                  </p>
                   <p className="text-sm mt-2 sm:text-base md:text-base text-gray-400">
                     {project.description}
                   </p>
                   <p className="text-sm mt-2 sm:text-base md:text-lg text-gray-100">
                     {project.domain}
                   </p>
+                  <div className="flex gap-4 mt-4">
+  <a
+    href={project.demo}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="px-4 py-2 bg-purple-600 hover:bg-purple-700 text-white rounded-lg shadow-md transition duration-300"
+  >
+    Live Demo
+  </a>
+  <a
+    href={project.link}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="px-4 py-2 border border-purple-600 text-purple-600 hover:bg-purple-600 hover:text-white rounded-lg shadow-md transition duration-300"
+  >
+    GitHub
+  </a>
+</div>
                 </div>
               </a>
             </div>
